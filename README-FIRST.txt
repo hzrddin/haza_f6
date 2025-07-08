5243f6f71db44fbffead1924565357016fca1398
Haza <hazaruddin2004@gmail.com>
2025-07-08 11:39:45 +0800

    Clear

--- README-FIRST.txt
+++
@@ -1,6 +0,0 @@
-To display this website, you open file "index" and open html file named as "main.html" / "main"
-
-filepath
-index > main
-
-That's all.
--- main.js
+++
@@ -1,7 +0,0 @@
-const elment = document.querySelector('.hamburger');
-const elment1 = document.querySelector('.nav-list');
-
-elment.addEventListener("click", ()=> {
-  elment.classList.toggle('active');
-  elment1.classList.toggle('active');
-})
